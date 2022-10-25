cc7fc2b8e697484d6815796e8bb1caa131745204
James Sullivan <jesullivan222@gmail.com>
2022-10-24 23:58:05 -0400

    Complete 04_removeFromArray exercise
    Learned how to get any number of arguments and loop through those
    arguments within a function.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    const removalArray = [];
+    for (i = 0; i < array.length; i++) {
+        for (arg of arguments) { // loops through any number of given arguments
+            if (array[i] === arg) {
+                array.splice(i, 1);
+                i--;
+                break; // breaks inner loop only
+            }
 
+        } 
+    }
+    return array;
 };
 
 // Do not edit below this line

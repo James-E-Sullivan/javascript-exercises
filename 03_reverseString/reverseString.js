25cfcf13b21a4244a963641f3aef769fd537695b
James Sullivan <jesullivan222@gmail.com>
2022-10-25 10:45:12 -0400

    Add TOP answer as comment for 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,6 @@
 const reverseString = function(string) {
+    // TOP answer: return string.split('').reverse().join('');
+
     let reversedString = '';
     for (i = string.length - 1; i >= 0; i--) {
         reversedString += string[i];

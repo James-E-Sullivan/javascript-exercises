cd845abbd8792d13a3dd8bac9ab3277bbd2b9313
James Sullivan <jesullivan222@gmail.com>
2022-11-01 10:47:49 -0400

    Complete 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let totalSum = 0;
+  for (let num of array) {
+    totalSum += num;
+  }
+  return totalSum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let answer = 1;
+  for (let num of array) {
+    answer *= num;
+  }
+  return answer;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) return 1;
+	let answer = a;
+  while (a > 1) {
+    answer *= a - 1;
+    a--;
+  }
+  return answer;
 };
 
 // Do not edit below this line

04823b26b47852ddedf8fd7c35cd56352552ff30
James Sullivan <jesullivan222@gmail.com>
2022-11-01 11:34:27 -0400

    Complete 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    let curr = 1;
+    let prev = 0;
+    for (let i = 0; i < n - 1; i++) {
+        let temp = curr;
+        curr = curr + prev;
+        prev = temp;
+    }
+    return curr;
 };
 
 // Do not edit below this line

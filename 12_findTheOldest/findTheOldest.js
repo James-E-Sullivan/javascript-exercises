df817f47c361b63c5212617c725cfcad30ffe04a
James Sullivan <jesullivan222@gmail.com>
2022-11-01 12:25:37 -0400

    Complete 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const oldest = array.reduce(function(prevOldest, currPerson, index) {
 
+        let prevOldestAge = prevOldest.yearOfDeath - prevOldest.yearOfBirth;
+        let currPersonAge = currPerson.yearOfDeath - currPerson.yearOfBirth;
+
+        const currentYear = new Date().getFullYear();
+
+        if (prevOldest.hasOwnProperty('yearOfDeath') !== true) {
+            prevOldestAge = currentYear - prevOldest.yearOfBirth;
+        }
+
+        if (currPerson.hasOwnProperty('yearOfDeath') !== true) {
+            currPersonAge = currentYear - currPerson.yearOfBirth;
+        }   
+
+        if (currPersonAge > prevOldestAge) {
+            return currPerson;
+        } else {
+            return prevOldest;
+        }
+    })
+    return oldest;
 };
 
 // Do not edit below this line

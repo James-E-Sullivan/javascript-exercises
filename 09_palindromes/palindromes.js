06f0ad809413e52c2dcbff90881b2ee2cac86643
James Sullivan <jesullivan222@gmail.com>
2022-11-01 11:06:49 -0400

    Complete 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const lowerStr = string.toLowerCase();
+    const lowerStrArray = Array.from(lowerStr);
+    const characterArray = lowerStrArray.filter(char => 
+        char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122);
 
+    for (let i = 0, j = characterArray.length - 1; i < j; i++, j--) {
+        if (characterArray[i] !== characterArray[j]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
